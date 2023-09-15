dc0272fb8c25cb79da4250cde4ddf202e58920c1
Biskweet <gaspard.dama@gmail.com>
2023-09-15 18:49:40 +0200

    Bigfix (see https://developer.mozilla.org/en-US/docs/Web/API/setTimeout)

--- tinylogger.ts
+++ tinylogger.ts
@@ -6,9 +6,9 @@ class TinyLogger {
      * Very minimal and stupid logger but I just want to put a term to this project and deploy
      */
 
-    private static logs: string[] = [];
+    public static logs: string[] = [];
 
-    private static lastTimeoutId: NodeJS.Timeout | null = null;
+    public static lastTimeoutId: NodeJS.Timeout | null = null;
 
     public static log(ip: string, query: string) {
         const now = new Date();
@@ -25,24 +25,24 @@ class TinyLogger {
     private static startDumpTimeout() {
         // If the buffer reaches 15 elements we dump regardless
         if (this.logs.length >= 15)
-            this.dumpLog();
+            this.dumpLog(this.logs);
 
         // If no timeout was planned we start one
         if (this.lastTimeoutId == null)
-            this.lastTimeoutId = setTimeout(this.dumpLog, config.logTimeoutDuration);
+            this.lastTimeoutId = setTimeout(this.dumpLog, config.logTimeoutDuration, this.logs);
 
         // A timeout was planned, refresh it
         else {
             clearTimeout(this.lastTimeoutId);
-            this.lastTimeoutId = setTimeout(this.dumpLog, config.logTimeoutDuration);
+            this.lastTimeoutId = setTimeout(this.dumpLog, config.logTimeoutDuration, this.logs);
         }
     }
 
-    private static dumpLog() {
-        fs.appendFileSync(config.logsFilename, this.logs.join('\n') + '\n');
+    private static dumpLog(logs: string[]) {
+        fs.appendFileSync(config.logsFilename, logs.join('\n') + '\n');
 
-        this.logs = [];
-        this.lastTimeoutId = null;
+        TinyLogger.logs = [];
+        TinyLogger.lastTimeoutId = null;
     }
 }
 
