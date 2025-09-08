6a711dad0b9cb93ee4228d78b093658419afd809
biskweet <gaspard.dama@gmail.com>
2025-09-08 21:35:50 +0200

    Fixed course code regex

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ export const config = {
     HOST: "localhost",
     PORT: 5001,
     CALDAVZAP_TOKEN: "c3R1ZGVudC5tYXN0ZXI6Z3Vlc3Q=",
-    regexCourseCode: /((MU|UM)[45][a-zA-Z0-9]{5})[ _-]+?([a-zA-Z]*)/,
+    regexCourseCode: /((MU|UM)[45][a-zA-Z0-9]{5})[ _-]+([^\s-]*)/,
     regexValidateQueryParams: /[a-zA-Z,]+/,
     logsFilename: "logs.txt",
     logTimeoutDuration: 2 * 60 * 1000,        // 2 min
